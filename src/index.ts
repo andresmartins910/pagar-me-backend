471e2a29e6184614229e0f787318883732d43cd4
andresmartins910 <andresmartins910@gmail.com>
2022-06-28 11:51:22 -0300

    update: added helmet

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 import fs from "fs";
 import path from "path";
 import routes from "./routes";
@@ -11,6 +12,9 @@ export const app = express();
 
 app.use(express.json());
 
+// helmet adds an extra layer of security
+app.use(helmet());
+
 // log middleware
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
@@ -19,6 +23,8 @@ const accessLogStream = fs.createWriteStream(
 
 app.use(morgan("combined", { stream: accessLogStream }));
 
+// routes
 routes(app);
 
+// error handler
 app.use(customErrorHandler);
